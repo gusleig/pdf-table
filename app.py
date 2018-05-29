6a4aeeed094fc8dac5f591cdc6bc31fc8123b0ab
Gustavo <gus.leig@gmail.com>
2018-05-29 02:22:18 -0300

    more options included

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #import tabula
 from tabula import read_pdf
 
-po = {'error_bad_lines': True, 'sep':';'}
+po = {'header': None, 'error_bad_lines': True, 'sep':';'}
 
 
-df = read_pdf("943703862.pdf", output_format="dataframe", encoding='utf-8', java_options=None, pandas_options=po, mutiple_tables=True, pages="all",guess=True)
+df = read_pdf("943703862.pdf", stream=True, lattice=False, output_format="dataframe", encoding='utf-8', java_options=None, pandas_options=po, mutiple_tables=True, pages="all",guess=False)
 
 df.to_csv('otuput.csv', encoding='utf-8')
