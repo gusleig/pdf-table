112af40499f0baa29b44787b816c3eceaf9c9fe5
Gustavo <gus.leig@gmail.com>
2018-05-30 17:22:30 -0300

    better Call Detail Record processing

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ def read_pdf_file():
 
 df = read_pdf_file()
 
+# export csv
+df.to_csv('output.csv',  sep=',', encoding='utf-8')
+
+
 cdr_obj = []
 
 anumber = ""
@@ -29,62 +33,63 @@ for index, row in df.iterrows():
     # get A number
 
     anumberstr = rowstr.replace(' ', '')
-    match = re.search(r'\((\d{2})\)\D*(\d{5})\D*([^,]*)', anumberstr)
 
-    if match:
-        anumber = match.group()
+    match_anumber = re.findall(r'\(\d{2}\)\D*\d{5}\D*[^,]*', str(anumberstr),re.MULTILINE)
+
+    if match_anumber:
+        anumber = ' '.join(match_anumber)
 
     # get index
-
-    match_index = re.search(r'^[0-9]{10}([,]*?)', rowstr)
+    # match_index = re.search(r'^[0-9]{10}([,]*?)', rowstr)
+    # find 10 digits betweens non digits
+    match_index = re.findall(r'(?<!\d)(\d{10})(?!\d)\s',rowstr)
 
     if match_index:
-        indx = match_index.group()
+        indx = match_index[0]
     else:
         indx = ""
 
-    match_date = re.search(r'(\d{2})/(\d{2})/(\d{4})', rowstr)
+    match_date = re.findall(r'\d{2}/\d{2}/\d{4}', rowstr)
 
     if match_date:
-        date = match_date.group()
+        date = match_date[0]
     else:
         date = ""
 
-    match_hour = re.search(r'\d{2}:\d{2}:\d{2}', rowstr)
+    match_hour = re.findall(r'\s(?:\d{1,2}:?)[0-5]\d:[0-5]\d', rowstr)
 
     if match_hour:
-        hour = match_hour.group()
+        hour = match_hour[0]
     else:
         hour = ""
 
-    match_dur = re.search(r'\,(\d{2}:\d{2}:\d{2})', rowstr)
+    match_dur = re.findall(r'\,(\d{2}:\d{2}:\d{2})', rowstr)
 
     if match_dur:
-        dur = match_dur.group(1)
+        dur = match_dur[0]
     else:
         dur = ""
 
-    match_numb = re.search(r'\D(\d{8,11}),', rowstr)
+    match_numb = re.findall(r'\D(\d{8,11}),', rowstr)
 
     if match_numb:
-        num_b = match_numb.group(1)
+        num_b = match_numb[0]
     else:
         num_b = ""
 
-    match_desc_serv1 = re.search(r'.*?,\s*(.*?),.*', rowstr)
+    match_desc_serv1 = re.findall(r'.*?,\s*(.*?),.*', rowstr)
 
     if match_desc_serv1:
-        desc_serv1 = match_desc_serv1.group(1)
+        desc_serv1 = match_desc_serv1[0]
     else:
         desc_serv1 = ""
 
     # look value between double quotes
-    match_val = re.search(r'''(["'])(?:(?=(\\?))\2.)*?\1''', rowstr)
+    match_val = re.findall(r'\"([0-9]+\D[1]*[0-9]+)\"', rowstr)
 
     if match_val:
-        val = match_val.group()
-        val = re.sub('"', '', val)
-
+        for i, val in enumerate(match_val):
+            match_val[i] = re.sub('"', '', val)
     else:
         val = ""
 
@@ -93,8 +98,12 @@ for index, row in df.iterrows():
     if match_index and match_date and match_dur and match_hour and match_numb:
         # generate CDR
         print("generates CDR OK")
-        cdr_obj.append({'index': indx, 'num_a': anumber, 'date': date, 'hour': hour,
-                        'duration': dur, 'num_b': num_b, 'desc_serv1': desc_serv1, 'val': val})
+        for i, val in enumerate(match_index):
+            try:
+                cdr_obj.append({'index': str(match_index[i]), 'num_a': anumber , 'date': str(match_date[i]), 'hour': str(match_hour[i].replace(' ', '')),
+                'duration': str(match_dur[i]), 'num_b': str(match_numb[i]), 'desc_serv1': str(match_desc_serv1[i]), 'val': str(match_val[i])})
+            except IndexError as e:
+                print(e)
 
     print(row[0])
 
@@ -102,4 +111,4 @@ df2 = pd.DataFrame(cdr_obj)
 
 
 df2.to_csv('output_clean.csv',  sep=',', encoding='utf-8')
-df.to_csv('output.csv',  sep=',', encoding='utf-8')
+
